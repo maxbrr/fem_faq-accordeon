f54b66c6559873e262b2732bbcc635c4e2a3db3f
Max Brückner <maxbrckner@gmail.com>
2024-01-09 18:07:57 +0100

    modiefied base URL in vite.config.js | re-build to /docs directory

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/fem_faq-accordeon/',
   resolve: {
     alias: {
       '@': '/src'
     },
   },
 });
-
-
